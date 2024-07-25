e64a1dda3ab6514706585bf832e279aae213bf66
TheCtkHoster <danirusachith@gmail.com>
2024-07-25 15:33:15 +0530

    remove (looks ugly)

--- visual.lua
+++ visual.lua
@@ -41,7 +41,7 @@ local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/d
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 
 local Window = Fluent:CreateWindow({
-    Title = "Visual [NURYSIUM RESKIN]" .. version,
+    Title = "Visual [NURYSIUM RESKIN]",
     SubTitle = "Edited",
     TabWidth = 160,
     Size = UDim2.fromOffset(580, 460),
